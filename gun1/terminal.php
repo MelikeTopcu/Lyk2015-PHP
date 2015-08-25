de1b5d5be5ef61a531e86fc54c48b2019d370d36
MelikeTopcu <melikeetopcuu@gmail.com>
2015-08-25 13:29:40 +0300

    first commit

---
+++ gun1/terminal.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8" />
+<?php
+
+echo "aysenur<br>";
+# <br> : alt satira gecmek icin
+
+echo "melike";
+?>
+
+<?php
+/*
+çok satırlı
+açıklama satırı
+kullanımı : php terminal.php melike
+ */
+// tek satırlık açıklama satırı
+# tek satırlık açıklama satırı
+echo "merhaba $argv[1] \n";
+print_r($argv);
+?>
